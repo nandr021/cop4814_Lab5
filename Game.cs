8c336179ee5bac62af1ab971cd2ea2c795d28b1f
nandr021 <37674399+nandr021@users.noreply.github.com>
2018-03-22 17:16:57 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Game
+    {
+        public string Team1;
+        public string Team2;
+        public int Score1;
+        public int Score2;
+
+        public Game()
+        {
+
+        }
+
+         public Game(string t1, string t2, int scr1, int scr2)
+        {
+            Team1 = t1;
+            Team2 = t2;
+            Score1 = scr1;
+            Score2 = scr2;
+        }
+
+        public override string ToString()
+        {
+            return Team1 + " ("+ Score1 + ") - " + Team2 + " (" + Score2 + ")";
+        }
+
+
+
+
+
+
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class GameFactory
+    {
+        List<Game> Gamelist = new List<Game>();
+
+        public List<Game> CreateGameList()
+        {
+            List<Game> lstCurrent = new List<Game>();
+
+        }
+
+
+
+
+    }
+}
